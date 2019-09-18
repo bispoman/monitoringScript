e4eae6fd69601bad19055de5b68b75e4e4fc64e5
bispoman <daniel@bisponet.com>
2019-09-17 21:48:01 -0300

    Updating the Readme, uploading the script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # monitoringScript
-a simple Python script to monitor a given URL and write a report on a CSV file.
+A simple Python script to monitor a given URL and write a report on a CSV file.
+
+This script was written using Python 3.7.4 and as is, monitors the URL: https://desafioperformance.b2w.io/bairros
+
+Every two seconds it makes a request to the URL above and after a full minute, a line will be written on the CSV file containing the timestamp, the count of 2xx responses, the count of 4xx responses and the count of 5xx responses. 
+If by the end of said minute the count of 4xx and 5xx is greater than the count of 2xx, the script will make a request to https://desafioperformance.b2w.io/reinicia to try to reset the service and bring it back online. 
+
+
+To run this script you'll need the following dependencies: 
+requests
+time
+datetime
+csv
+
+All of them can be installed via PIP or your favorite package manager.
+
+In case of any doubt, feel free to submit it as an issue. 
+
+Thanks
---
+++ monitoring.py
@@ -0,0 +1,28 @@
+import requests
+import time
+import datetime
+import csv
+with open('bairrosReport.csv', 'w', newline='') as f:
+    filewriter = csv.writer(f)
+    
+    filewriter.writerow(['timestamp', '2XX status count', '4XX status count', '5XX status count'])
+
+    while True:
+        startTime = time.time()
+        while startTime < startTime+60: 
+            status2 = 0
+            status4 = 0
+            status5 = 0
+            response = requests.get("https://desafioperformance.b2w.io/bairros")
+            status = str(response.status_code)[:1]
+            if status == 2: 
+                status2 = status2 + 1
+            if status == 4:
+                status4 = status4 + 1
+            if status == 5:
+                status5 = status5 + 1
+            time.sleep(2)
+
+        if (status4 + status5) > status2:
+            requests.put("https://desafioperformance.b2w.io/reinicia")
+        filewriter.writerow([datetime.fromtimestamp(startTime), status2, status4, status5])
